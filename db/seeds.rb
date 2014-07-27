ddcaad2b7ed779da652fb2c931f14639c0466bd9
kagemusha <mmadrid99@gmail.com>
2014-07-27 13:07:14 -0400

    sample user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
-# Environment variables (ENV['...']) can be set in the file .env file.
+
+if Rails.env == "development"
+  user = User.create! :name => "Tester", :email => "t@t.com", :password => "tester", :password_confirmation =>"tester"
+end
