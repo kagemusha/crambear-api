a085f52908eacd6db10979d2b07353550234c45d
kagemusha <mmadrid99@gmail.com>
2015-08-17 15:29:24 -0400

    include tags when GET card_set cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,3 +1,18 @@
-class CardsController < JsonApiController
+    class CardsController < JsonApiController
 
-end
+      def get_related_resources
+        if (params[:source] == 'card_sets')
+          card_set = CardSet.find params[:card_set_id]
+          card_serializer = JSONAPI::ResourceSerializer.new(
+              CardResource, include: ['tags'],
+              fields: {
+                  tags: [:name]
+              }
+          )
+          cards_json = card_serializer.serialize_to_hash(card_set.cards.map { |card| CardResource.new card})
+          render json: cards_json.to_json
+        else
+          super
+        end
+      end
+    end
